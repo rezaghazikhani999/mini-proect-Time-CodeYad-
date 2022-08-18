4ae2f9c09b3ee0fabdcf3f761f266c11b0d8c6ca
rezaghazi <rezaghazi8800@gmail.com>
2022-08-18 12:18:40 +0430

    mini project TIME codeyad

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const tick = () => {
+class Timer extends React.Component {
+constructor(){
+ super();
+ this.state={
+  time : new Date().toLocaleTimeString()
+ } 
+}
+  render() {
+
+    
+    setInterval(() =>{
+     this.setState({
+      time:new Date().toLocaleTimeString()
+     })
+    },1000)
+
+
+    return (
+      <div>
+        <h3 className="timer">it is{this.state.time}</h3>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <h1>My Timer</h1>
+        <Timer />
+      </div>
+    );
+  }
+}
+// const tick = () => {
   const elem = root.render(
     <div>
-      <h1>Hi fetens</h1>
-      <h3>it is{new Date().toLocaleTimeString()}</h3>
+      <App />
     </div>
   );
-};
+// };
 
-setInterval(() => {
-  tick();
-}, 1000);
+// setInterval(() => {
+//   tick();
+// }, 1000);
